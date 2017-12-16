d73b5aa12ed700ed0f8bba3a0996aa5c5d7a1d62
saksham2402 <sakshamsinhafeb24@gmail.com>
2017-12-16 19:58:27 +0530

    task_0 committed

---
+++ app/src/main/java/saksham/com/helloworld1/MainActivity.java
@@ -0,0 +1,13 @@
+package saksham.com.helloworld1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
